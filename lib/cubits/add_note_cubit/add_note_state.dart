e1bf5f347d07bde0ee43442ec4d096585321c031
Youssef Refaat <yossifrefaat40@gmail.com>
2024-01-08 19:33:34 +0200

    Create add note state

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -4,3 +4,12 @@ part of 'add_note_cubit.dart';
 sealed class AddNoteState {}
 
 final class AddNoteInitial extends AddNoteState {}
+
+final class AddNoteLoading extends AddNoteState {}
+
+final class AddNoteSuccess extends AddNoteState {}
+
+final class AddNoteFalure extends AddNoteState {
+  final String errorMessage;
+  AddNoteFalure(this.errorMessage);
+}
