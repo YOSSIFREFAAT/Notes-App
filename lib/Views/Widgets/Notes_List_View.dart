f7c69e1b952f21e2d16b4a719cb17267abcad608
Youssef Refaat <yossifrefaat40@gmail.com>
2024-01-09 11:38:04 +0200

    Refresh add/delete note

--- lib/Views/Widgets/Notes_List_View.dart
+++ lib/Views/Widgets/Notes_List_View.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/Views/Widgets/note_item.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
-import 'package:notes_app/notes_cubit/notes_cubit.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
--- lib/Views/Widgets/Notes_View_body.dart
+++ lib/Views/Widgets/Notes_View_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:notes_app/notes_cubit/notes_cubit.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/Widgets/custom_appbar.dart';
 import 'package:notes_app/views/Widgets/notes_list_view.dart';
 
