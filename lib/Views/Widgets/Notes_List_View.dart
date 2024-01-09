2f0037235f9d29d09816ea68bfea94d6eed10ba5
Youssef Refaat <yossifrefaat40@gmail.com>
2024-01-09 10:51:37 +0200

    Fix notes cubit by calling fetch method

--- lib/Views/Widgets/Notes_List_View.dart
+++ lib/Views/Widgets/Notes_List_View.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/Views/Widgets/note_item.dart';
+import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/notes_cubit/notes_cubit.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        physics: const BouncingScrollPhysics(),
-        itemBuilder: (context, index) => const Padding(
-          padding: EdgeInsets.symmetric(vertical: 8.0),
-          child: NoteItem(),
-        ),
-      ),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes!;
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: ListView.builder(
+            padding: EdgeInsets.zero,
+            physics: const BouncingScrollPhysics(),
+            itemCount: notes.length,
+            itemBuilder: (context, index) => const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child: NoteItem(),
+            ),
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/notes_cubit/notes_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:meta/meta.dart';
+import 'package:notes_app/constans.dart';
+import 'package:notes_app/models/note_model.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+  List<NoteModel>? notes;
+  fitchAllNotes() {
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    notes = notesBox.values.toList();
+  }
+}
